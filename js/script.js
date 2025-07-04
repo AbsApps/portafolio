40ca1e98051341290dad5f8dbcad75da482146b8
Chris <absadev05@gmail.com>
2025-07-04 00:32:42 -0600

    Upgade to boostrap

--- js/script.js
+++ js/script.js
@@ -1,83 +1,54 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const mobileMenuButton = document.getElementById('mobile-menu-button');
-    const mobileMenu = document.getElementById('mobile-menu');
-
-    mobileMenuButton.addEventListener('click', function () {
-        // Alterna la clase 'hidden' para mostrar/ocultar el menú
-        mobileMenu.classList.toggle('hidden');
-        // También puedes añadir clases para animaciones aquí si quieres
-    });
-
-    // Opcional: Cerrar el menú si se hace clic fuera de él (para mejorar la UX)
-    document.addEventListener('click', function (event) {
-        if (!mobileMenu.contains(event.target) && !mobileMenuButton.contains(event.target) && !mobileMenu.classList.contains('hidden')) {
-            mobileMenu.classList.add('hidden');
-        }
-    });
-});
-
-
-const swiper = new Swiper('.swiper.cards', {
+var swiper = new Swiper(".myCardsPictures", {
     effect: "cards",
     grabCursor: true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
-    // Optional parameters
-    // direction: 'vertical',
-    // loop: true,
-
-    // If we need pagination
-    // pagination: {
-    //     el: '.swiper-pagination',
-    // },
-
-    // // Navigation arrows
-    // navigation: {
-    //     nextEl: '.swiper-button-next',
-    //     prevEl: '.swiper-button-prev',
-    // },
-
-    // // And if we need scrollbar
-    // scrollbar: {
-    //     el: '.swiper-scrollbar',
-    // },
-});
-
-
-
-const swiperCredentials = new Swiper(".swiper.credentials", {
-    slidesPerView: 4,
-    spaceBetween: 4,
-    freeMode: true,
-          autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
     },
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    // },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+
+
+var swiper = new Swiper(".myCertifications", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    // },
     breakpoints: {
-        300: {
-            slidesPerView: 2,
-            spaceBetween: 5,
+        340: {
+            slidesPerView: 1,
+            spaceBetween: 8,
         },
         640: {
             slidesPerView: 2,
-            spaceBetween: 2,
+            spaceBetween: 8,
         },
         768: {
             slidesPerView: 2,
-            spaceBetween: 4,
+            spaceBetween: 40,
         },
         1024: {
             slidesPerView: 4,
-            spaceBetween: 4,
-        }
-    }
+            spaceBetween: 40,
+        },
+    },
 });
+
+console.log('myCertifications')
 
 
 // AOS INIT
